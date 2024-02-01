7dc298442ea61354d801c39df58292ecf20259a1
Shibraj <shibrajdeb456@gmail.com>
2024-02-01 23:31:03 +0530

    started pandas

---
+++ pandas.py
@@ -0,0 +1,24 @@
+# import pandas as pd 
+# import numpy as np
+# df=pd.read_csv('income.csv')
+# # print(df.head())
+
+# import pandas as pd
+# import pandas as pd
+
+# # Calling DataFrame constructor
+# df = pd.DataFrame()
+
+# print(df)
+
+
+# import pandas as pd
+# import pandas as pd
+
+# # list of strings
+# lst = ['Geeks', 'For', 'Geeks', 'is', 
+# 			'portal', 'for', 'Geeks']
+
+# # Calling DataFrame constructor on list
+# df = pd.DataFrame(lst)
+# print(df)
